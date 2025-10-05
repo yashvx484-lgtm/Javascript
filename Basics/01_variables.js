37cd1a599bbc0a62a636aa1d58978938ab03b16b
Yash Vaish <yashvx484@gmail.com>
2025-10-05 11:59:17 +0530

    Second Commit

---
+++ Basics/01_variables.js
@@ -0,0 +1,10 @@
+let email = "yashvaish129@gmail.com"; //let is for variable declaration
+var name = "YASHVAISH"; //not in use 
+Number = 9592937832;  //automatic variable assigned
+const pincode=121212;   //constant
+console.log(email); 
+console.log(name); //single line print
+console.log(Number);
+console.log(pincode);
+
+console.table([email, name, Number,pincode]); /*tablular print */
