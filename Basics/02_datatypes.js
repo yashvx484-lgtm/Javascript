3402c48b01b4666c93b74403e8ff26b5ad157460
Yash Vaish <yashvx484@gmail.com>
2025-10-06 15:12:23 +0530

    NEW LEARNING

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let age=34
+let name="yash"
+let login=false
+
+let unknown= undefined
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof unknown);
+console.log(typeof login);
