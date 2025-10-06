92909894f49092d5775505c501cf682c3f48e78f
Yash Vaish <yashvx484@gmail.com>
2025-10-06 16:05:02 +0530

    New Learning

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let XYZ="YASH"
+
+console.log(typeof XYZ)
+console.log(XYZ)
+
+
+let convofnew= Number(XYZ) //conversion
+console.log(typeof convofnew) //type of conversion
+console.log(convofnew)
+
+//trying more examples for better clarity
+
+let IsLogin= 1
+
+console.log(typeof IsLogin)
+console.log( IsLogin)
+
+let convofLogin= Boolean(IsLogin);
+console.log(typeof convofLogin)
+
+//number to boolean conversion
+
+
